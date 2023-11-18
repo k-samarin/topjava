a4bd98697ec3bc9d182b1da10d98fe569f2f1b38
Kostya Samarin <ksamarin@gmail.com>
2023-11-18 07:57:40 +0300

    Remove testSkipper usage in tests

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcMealServiceTest.java
@@ -1,15 +1,17 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Rule;
-import org.junit.rules.MethodRule;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.TestSkipper;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcMealServiceTest extends AbstractMealServiceTest {
-    @Rule
-    public MethodRule testSkipper = new TestSkipper("createWithException");
+    @Ignore
+    @Test
+    @Override
+    public void createWithException() {
+    }
 }
--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,10 +1,9 @@
 package ru.javawebinar.topjava.service.jdbc;
 
-import org.junit.Rule;
-import org.junit.rules.MethodRule;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.TestSkipper;
 import ru.javawebinar.topjava.service.AbstractUserServiceNoCacheTest;
 
 import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
@@ -13,6 +12,10 @@ import static ru.javawebinar.topjava.Profiles.JDBC;
 @DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceNoCacheTest {
-    @Rule
-    public MethodRule testSkipper = new TestSkipper("createWithException");
+    @Ignore
+    @Test
+    @Override
+    public void createWithException() throws Exception {
+    }
+
 }
